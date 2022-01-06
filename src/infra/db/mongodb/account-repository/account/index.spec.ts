13f13bd2c580ada459bad54377dac0df836954cd
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-01-06 16:32:37 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/db/mongodb/account-repository/account/index.spec.ts
+++ src/infra/db/mongodb/account-repository/account/index.spec.ts
@@ -10,8 +10,17 @@ describe('Account mongo Respoitory', () => {
     await MongoHelper.disconnect();
   });
 
+  // interface makeSutTypes {
+  //   sut: AccountMongoRepository;
+
+  // }
+
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository();
+  };
+
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository();
+    const sut = makeSut();
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
