5a00536c0c7b82d9fbd57a880e107795ddac8953
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-02-01 00:54:22 -0300

    refactor: change the file name

--- src/main/config/app/index.ts
+++ src/main/config/app/index.ts
@@ -1,10 +1,7 @@
-import { cors, ContentType } from '@main/middlewares';
 import express from 'express';
 import setupMiddlewares from '../middlewares';
 
 const app = express();
-app.use(cors);
-app.use(ContentType);
 setupMiddlewares(app);
 
 export { app };
--- src/main/middlewares/content-type/index.ts
+++ src/main/middlewares/content-type/index.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
-const ContentType = (req: Request, res: Response, next: NextFunction): void => {
+const contentType = (req: Request, res: Response, next: NextFunction): void => {
   res.type('json');
 
   next();
 };
 
-export { ContentType };
+export { contentType };
