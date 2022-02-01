93e91ece8857c9bfbfe84fbde2e547bde0ae6be6
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-02-01 00:20:24 -0300

    feat: ensure request return json as default content type

--- src/main/config/app/index.ts
+++ src/main/config/app/index.ts
@@ -1,9 +1,10 @@
-import { cors } from '@main/middlewares/cors';
+import { cors, ContentType } from '@main/middlewares';
 import express from 'express';
 import setupMiddlewares from '../middlewares';
 
 const app = express();
 app.use(cors);
+app.use(ContentType);
 setupMiddlewares(app);
 
 export { app };
